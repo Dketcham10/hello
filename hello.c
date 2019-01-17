7a89721f3825472355c213a545159a910a5ee68a
Dillon Ketcham <dketcham10@gmail.com>
2019-01-17 12:57:22 -0600

    Initial commit

--- hello.c
+++ hello.c
@@ -1,1 +1,16 @@
-/***  Author: Chris Bourke*  Date: 2016/11/02**  A simple hello world program in C**/#include<stdlib.h>#include<stdio.h>int main(int argc, char **argv) {printf("Hello World!\n");return 0;}
+/**
+ * Author: Dillon Ketcham
+ * Partner: Kevin Escomilla
+ * Date: 2019/01/17
+ *
+ * A simple hello world program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+int main(int argc, char **argv) {
+
+  printf("Dillon Ketcham\n");
+
+  return 0;
+}
